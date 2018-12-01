d5beab6ce1465a83f0b6bd4d2323c956112281b0
Ray Ashman Jr <ray@rayashmanjr.com>
2018-12-01 14:03:24 -0500

    Just one file handle

--- day1/src/main.rs
+++ day1/src/main.rs
@@ -5,27 +5,27 @@ use std::fs::File;
 use std::collections::HashMap;
 
 fn main() {
-    let file = File::open("inputs/part-1.txt").expect("Dang");
-    let result = calculate_frequency(file);
+    let file = File::open("inputs/part-1.txt").expect("Couldn't open input!");
+    let result = calculate_frequency(&file);
     println!("Part 1: {}", result);
 
-    let file = File::open("inputs/part-1.txt").expect("Dang");
-    let result = calculate_loop_frequency(file);
+    let result = calculate_loop_frequency(&file);
     println!("Part 2: {}", result);
 }
 
-fn calculate_frequency(file: File) -> isize {
+fn calculate_frequency(file: &File) -> isize {
     let reader = BufReader::new(file);
     reader.lines().fold(0, |sum, line| sum + line.unwrap().parse::<isize>().unwrap())
 }
 
-fn calculate_loop_frequency(file: File) -> isize {
+fn calculate_loop_frequency(file: &File) -> isize {
     let mut reader = BufReader::new(file);
     let mut seen: HashMap<isize, bool> = HashMap::new();
-    let mut frequency = 0;
+    let mut frequency: isize = 0;
 
     loop {
-        reader.seek(SeekFrom::Start(0)).expect("Failed to reset");
+        reader.seek(SeekFrom::Start(0)).expect("Failed to seek to start of file!");
+
         for line in reader.by_ref().lines() {
             frequency += line.unwrap().parse::<isize>().unwrap();
 
